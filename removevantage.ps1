9c18e448d760987e24e5ea96b14c2875acfb7679
Frank Maxwitat <maxwitat@gmx.de>
2023-03-12 12:01:32 +0100

    Add files via upload

---
+++ removevantage.ps1
@@ -0,0 +1,16 @@
+﻿#RemoveVantage
+$LittleLogFile = "c:\windows\Logs\RemoveVantageService.log"
+"Searching Vantage Service Installation" | Out-File $LittleLogFile
+
+$UninstallString = Get-ChildItem -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall" | Get-ItemProperty | Where-Object {$_.DisplayName -like "Lenovo Vantage Service"} | Select-Object -ExpandProperty QuietUninstallString
+
+if($UninstallString)
+{
+    "Calling $UninstallString " | Out-File $LittleLogFile -append
+    cmd /c $UninstallString
+}
+else
+{
+    "No installation found" | Out-File $LittleLogFile -append
+}
+
